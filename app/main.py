67382f0f7ab9169627d40406fd9c72c91b35ed4e
Raghav Wahi <raghavwahi7@gmail.com>
2024-07-10 02:55:38 +0530

    - Add loading of env into app - Add logging setup in main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
+from app.util.logger import initialize_logging
+import logging
+
+load_dotenv()
+initialize_logging()
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return {"message": "Application is up and running."}
+    message = "Application is up and running."
+    logging.info(message)
+
+    return {"message": message}
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app)

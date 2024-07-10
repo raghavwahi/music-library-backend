17ef74652636ded42075bacde941fc00461e7ae4
Raghav Wahi <raghavwahi7@gmail.com>
2024-07-10 23:46:49 +0530

    - Add GET /songs/:title API

--- app/routes/songs.py
+++ app/routes/songs.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, status, HTTPException
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
 from app.engine import schemas, database, models
@@ -14,3 +15,11 @@ def read_songs(skip: int = Query(0, ge=0), limit: int = Query(10, ge=0), db: Ses
     total = db.query(models.Song).count()
     songs = db.query(models.Song).offset(skip).limit(limit).all()
     return schemas.SongPagination(total=total, skip=skip, limit=limit, data=songs)
+
+
+@router.get("/{title}", response_model=schemas.Song)
+def read_song(title: str, db: Session = Depends(database.get_db)):
+    song = db.query(models.Song).filter(func.lower(models.Song.title) == func.lower(title)).first()
+    if song is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"song not found")
+    return song

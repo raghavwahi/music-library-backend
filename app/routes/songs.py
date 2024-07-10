fea8b932846b009c5328c6d3aa5e7070e6034ac0
Raghav Wahi <raghavwahi7@gmail.com>
2024-07-10 23:53:09 +0530

    - Add PUT /songs/:id API

--- app/routes/songs.py
+++ app/routes/songs.py
@@ -23,3 +23,16 @@ def read_song(title: str, db: Session = Depends(database.get_db)):
     if song is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"song not found")
     return song
+
+
+@router.put("/{song_id}", response_model=schemas.Song)
+def update_song(song_id: str, song: schemas.Song, db: Session = Depends(database.get_db)):
+    song = db.query(models.Song).filter(models.Song.id == song_id).first()
+    if song is None:
+        raise HTTPException(status_code=404, detail="Song not found")
+    for var, value in vars(song).items():
+        if value is not None:
+            setattr(song, var, value)
+    db.commit()
+    db.refresh(song)
+    return song

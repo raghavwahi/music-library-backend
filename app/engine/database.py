e4a99357ba99a4fd9abec6cc6f6159006913b5c9
Raghav Wahi <raghavwahi7@gmail.com>
2024-07-10 21:52:22 +0530

    - Add database.py

---
+++ app/engine/database.py
@@ -0,0 +1,15 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(os.environ.get("DATABASE_URL"), connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

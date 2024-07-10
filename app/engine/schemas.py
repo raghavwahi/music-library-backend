c080514162a4c6f7319042dd70ecbadb53c09cea
Raghav Wahi <raghavwahi7@gmail.com>
2024-07-10 21:52:37 +0530

    - Add schemas.py with Song pydantic Schema

---
+++ app/engine/schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+
+
+class Song(BaseModel):
+    id: str
+    title: str
+    danceability: float
+    energy: float
+    key: int
+    loudness: float
+    mode: int
+    acousticness: float
+    instrumentalness: float
+    liveness: float
+    valence: float
+    tempo: float
+    duration_ms: int
+    time_signature: int
+    num_bars: int
+    num_sections: int
+    num_segments: int
+    _class: int = Field(alias="class")
+    rating: int
